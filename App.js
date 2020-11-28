f491de57c376284622083febf6614105297d184e
Mubbashir Ali <mubbashirali35@gmail.com>
2020-11-28 22:42:54 +0500

    All Screen Components added

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet, ImageBackground, StatusBar } from 'react-native';
-import { Item, Input, Button } from 'native-base';
+import { View, Text, SafeAreaView, StyleSheet, ImageBackground, 
+         StatusBar, KeyboardAvoidingView, ScrollView, TouchableOpacity } from 'react-native';
+import { Item, Input } from 'native-base';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
   return(
@@ -10,12 +13,14 @@ const App = () => {
 
       <View style={styles.mainContentContainer}>
         
-        <View style={{ height: '30%' }}>
+        <View style={{ height: '30%', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between' }}>
           <Text style={{ color: '#fff', fontWeight: 'bold', fontSize: 40 }}>
             Sign Up
           </Text>
+          <TouchableOpacity>
+            <FontAwesomeIcon icon={faPlusCircle} size={100} style={{ color: '#fff' }} />
+          </TouchableOpacity>
         </View>
-
         <View style={{ width: '100%', height: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
           <Item rounded style={{ backgroundColor: '#fff', paddingLeft: 10, height: '18%' }}>
             <Input placeholder='Full Name' />
@@ -35,12 +40,14 @@ const App = () => {
 
         </View>
 
-        <Button rounded style={{ width: '100%', backgroundColor: '#fff',
-           alignItems: 'center', flexDirection: 'column' }}>
+        <TouchableOpacity style={{ width: '100%', height: '9%', backgroundColor: '#fff',
+           alignItems: 'center', flexDirection: 'column', justifyContent: 'center',
+           borderRadius: 30 }} onPress={() => console.log('Hello')} activeOpacity={0.5}
+             >
           <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#f88a00' }} >
             Sign Up
           </Text>
-        </Button>
+        </TouchableOpacity>
 
       </View>
 
@@ -60,7 +67,8 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    minHeight: 640
   },
   mainContentContainer: {
     width: '90%',
